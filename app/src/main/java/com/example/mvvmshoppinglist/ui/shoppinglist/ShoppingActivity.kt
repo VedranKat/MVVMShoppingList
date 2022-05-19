4ddc8ba891fe83505b296fbfcfc643ce1cd68940
unknown <vedstandard@runbox.com>
2022-05-19 03:11:36 +0200

    crud finished //ADD DI

--- app/src/main/java/com/example/mvvmshoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/mvvmshoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -3,12 +3,17 @@ package com.example.mvvmshoppinglist.ui.shoppinglist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Property.of
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvmshoppinglist.R
 import com.example.mvvmshoppinglist.data.db.ShoppingDatabase
+import com.example.mvvmshoppinglist.data.db.entities.ShoppingItem
 import com.example.mvvmshoppinglist.data.repositories.ShoppingRepository
 import com.example.mvvmshoppinglist.databinding.ActivityShoppingBinding
+import com.example.mvvmshoppinglist.other.ShoppingItemAdapter
 import java.util.Optional.of
 
 class ShoppingActivity : AppCompatActivity() {
@@ -18,13 +23,37 @@ class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityShoppingBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_shopping)
+        setContentView(binding.root)
 
-        val database = ShoppingDatabase(this) //bad practice
+        //bad practice, do DI
+        val database = ShoppingDatabase(this)
         val repository = ShoppingRepository(database)
         val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
+
+        val adapter = ShoppingItemAdapter(listOf(), viewModel)
+
+        binding.rvShoppingItems.layoutManager = LinearLayoutManager(this)
+        binding.rvShoppingItems.adapter = adapter
+
+        viewModel.getAllShoppingItems().observe(this, Observer{
+            //observe is called whenever livedata changes
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
+
+
+        binding.fab.setOnClickListener(){
+            AddShoppingItemDialog(this,
+            object: AddDialogListener{
+                override fun onAddButtonClicked(item: ShoppingItem) {
+                    viewModel.upsert(item)
+                }
+            }).show()
+        }
+
+
 
 
     }
